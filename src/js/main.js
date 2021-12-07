60c10469cc54a281b8122c7108eb3d45f1109f37
Nail <nailgalimov8@gmail.com>
2021-12-07 15:16:36 +0300

    desctop

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 import documentReady from "./helpers/documentReady";
 import lazyImages from "./modules/lazyImages";
+import map from "./modules/map";
 
 documentReady(() => {
 	lazyImages();
+	map();
 });
