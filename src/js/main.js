45b15935ea398953f1c2c99fddf920c113e01df3
Nail <nailgalimov8@gmail.com>
2021-12-08 16:49:51 +0300

    upd

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,10 @@
 import documentReady from "./helpers/documentReady";
 import lazyImages from "./modules/lazyImages";
 import map from "./modules/map";
-import indexPageAnim from "./modules/indexPageAnim";
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+import indexPageStartAnim from "./modules/indexPageStartAnim";
 import initDropMenu from "./modules/initDropMenu";
 import showAccordionBg from "./modules/showAccordionBg";
+import indexPageAnim from "./modules/indexPageAnim";
 
 window.onload = function() {
 	let preloader = document.querySelector(".preloader");
@@ -13,9 +12,8 @@ window.onload = function() {
 	setTimeout(() => {
 		preloader.classList.add("preloader_is-loaded");
 
-		indexPageAnim();
+		indexPageStartAnim();
 	}, 300);
-
 }
 
 documentReady(() => {
@@ -23,143 +21,5 @@ documentReady(() => {
 	map();
 	initDropMenu();
 	showAccordionBg();
-
-	gsap.registerPlugin(ScrollTrigger);
-
-	gsap.set(".js-about-company-info-block", { x: "-66%" })
-	gsap.set(".contact-us__content-with-arrow", { x: "-50%" })
-
-	let projectLayers = gsap.utils.toArray(".js-animated-layer");
-
-	// анмиация в о компании
-	let tl = gsap.timeline({
-		scrollTrigger: {
-			trigger: ".js-about-company-info-block",
-			start: "top bottom",
-			end: "top 60%",
-			scrub: true,
-			scrub: 5,
-		}
-	});
-
-	tl.to(".js-about-company-info-block", {
-		x: 0
-	})
-
-	// анмиация в контактах
-	let tl2 = gsap.timeline({
-		scrollTrigger: {
-			trigger: ".contact-us__content-with-arrow",
-			start: "top bottom",
-			scrub: true,
-			scrub: 1,
-		}
-	});
-
-	tl2.to(".contact-us__content-with-arrow", {
-		x: 0
-	})
-
-	// анмиация цифр о компании
-	gsap.set(".js-company-numbers1", { maxWidth: "100%" })
-
-	let tl3 = gsap.timeline({
-		scrollTrigger: {
-			trigger: ".js-company-numbers1",
-			start: "top 70%",
-			scrub: true,
-			scrub: 1,
-		}
-	});
-
-	tl3.to(".js-company-numbers1", {
-		maxWidth: "66.3%"
-	})
-	gsap.set(".js-company-numbers2", { maxWidth: "90%" })
-
-	let tl4 = gsap.timeline({
-		scrollTrigger: {
-			trigger: ".js-company-numbers2",
-			start: "top bottom",
-			scrub: true,
-			scrub: 1,
-		}
-	});
-
-	tl4.to(".js-company-numbers2", {
-		maxWidth: "32.6%"
-	})
-
-	// линии в секции направления
-	gsap.utils.toArray(".js-direction-lines").forEach((el)=>{
-		gsap.set(el, { x: "-100%" })
-	})
-
-
-	gsap.utils.toArray(".js-direction-lines").forEach((el)=>{
-		let tl5 = gsap.timeline({
-			scrollTrigger: {
-				trigger: ".js-direction-lines"
-			}
-		});
-
-		tl5.to(el, {
-			x: 0,
-			duration: 0.3,
-			delay: 0.3
-		})
-	})
-
-
-	// текст в секции почему нас выбирают
-	gsap.utils.toArray(".why-us__bottom-block-text").forEach((el)=>{
-		gsap.set(el, { x: "-5.5rem" })
-	})
-
-
-	gsap.utils.toArray(".why-us__bottom-block-text").forEach((el)=>{
-		let tl6 = gsap.timeline({
-			scrollTrigger: {
-				trigger: el,
-				start: "top bottom",
-				scrub: true,
-				scrub: 1,
-			}
-		});
-
-		tl6.to(el, {
-			x: 0,
-		})
-	})
-
-	function animFrom(elem, direction) {
-		direction = direction || -1;
-		var x = 0,
-			y = direction * 100;
-		gsap.fromTo(elem, {
-			y: 0,
-		}, {
-			delay: 0.6,
-			duration: 1.25,
-			y: `${y}%`,
-			ease: "expo",
-			overwrite: "auto"
-		});
-	}
-
-	projectLayers.forEach((el)=>{
-
-		ScrollTrigger.create({
-			trigger: el,
-			onEnter: function () {
-				animFrom(el)
-			},
-			onEnterBack: function () {
-				animFrom(el, 1)
-			},
-			onLeave: function () {
-				gsap.set(el, { y: "100%" })
-			}
-		});
-	})
+	indexPageAnim();
 });
--- src/js/modules/indexPageAnim.js
+++ src/js/modules/indexPageAnim.js
@@ -1,25 +1,155 @@
 import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 export default () => {
-	// let headerLinks = gsap.utils.toArray(".js-animated-el");
+	gsap.registerPlugin(ScrollTrigger);
 
-	gsap.set(".hero__layer", { scaleX: 1.2, scaleY: 1.2 });
-	gsap.set(".hero__info", { y: 75, opacity: 0 });
-	gsap.set(".js-animated-el", { y: -50, opacity: 0 });
-	gsap.set(".hero__text-span", { opacity: 0 });
-	gsap.set(".hero__long-arrow_wrap", { width: "6rem" });
-	gsap.set(".hero__feautures-item:nth-child(1)", { x: "6rem", opacity: 0 });
-	gsap.set(".hero__feautures-item:nth-child(2)", {  x: "-10rem", opacity: 0 });
+	if (document.querySelector(".js-index-page")) {
+		ScrollTrigger.matchMedia({
+			"(min-width: 767px)": function () {
+				gsap.set(".js-about-company-info-block", { x: "-66%" })
+				gsap.set(".contact-us__content-with-arrow", { x: "-50%" })
 
-	let timeline = gsap.timeline();
+				// анмиация в о компании
+				let tl = gsap.timeline({
+					scrollTrigger: {
+						trigger: ".js-about-company-info-block",
+						start: "top bottom",
+						end: "top 60%",
+						scrub: true,
+						scrub: 5,
+					}
+				});
 
-	timeline
-		.to(".hero__layer", { scaleX: 1, scaleY: 1, duration: 2, ease: "power4.out", clearProps: "scale"})
-		.to(".hero__feautures-item:nth-child(1)", { x: "0", opacity: 1, duration: 3, ease: "power4.out", clearProps: "all"}, "-=1.6")
-		.to(".hero__feautures-item:nth-child(2)", { x: "0", opacity: 1, duration: 3.3, ease: "power4.out", clearProps: "all"}, "-=3")
-		.to(".hero__text-span", { opacity: 1, duration: 2, ease: "power4.out", clearProps: "all"}, "-=2.9")
-		.to(".hero__long-arrow_wrap", { width: "20.5rem", duration: 2, ease: "power4.out", clearProps: "all"}, "-=2.9")
-		.to(".js-animated-el", { y: 0, opacity: 1, duration: 2.5, ease: "power4.out", clearProps: "all" }, "-=1")
-		.to(".hero__info", { y: 0, opacity: 1, duration: 2.5, ease: "power4.out", clearProps: "all" }, "-=2.5")
+				tl.to(".js-about-company-info-block", {
+					x: 0
+				})
+
+				// анмиация в контактах
+				let tl2 = gsap.timeline({
+					scrollTrigger: {
+						trigger: ".contact-us__content-with-arrow",
+						start: "top bottom",
+						scrub: true,
+						scrub: 1,
+					}
+				});
+
+				tl2.to(".contact-us__content-with-arrow", {
+					x: 0
+				})
+
+				// анмиация цифр о компании
+				gsap.set(".js-company-numbers1", { maxWidth: "100%" })
+
+				let tl3 = gsap.timeline({
+					scrollTrigger: {
+						trigger: ".js-company-numbers1",
+						start: "top 70%",
+						scrub: true,
+						scrub: 1,
+					}
+				});
+
+				tl3.to(".js-company-numbers1", {
+					maxWidth: "66.3%"
+				})
+				gsap.set(".js-company-numbers2", { maxWidth: "90%" })
+
+				let tl4 = gsap.timeline({
+					scrollTrigger: {
+						trigger: ".js-company-numbers2",
+						start: "top bottom",
+						scrub: true,
+						scrub: 1,
+					}
+				});
+
+				tl4.to(".js-company-numbers2", {
+					maxWidth: "32.6%"
+				})
+
+				// линии в секции направления
+				gsap.utils.toArray(".js-direction-lines").forEach((el) => {
+					gsap.set(el, { x: "-100%" })
+				})
+
+
+				gsap.utils.toArray(".js-direction-lines").forEach((el) => {
+					let tl5 = gsap.timeline({
+						scrollTrigger: {
+							trigger: ".js-direction-lines"
+						}
+					});
+
+					tl5.to(el, {
+						x: 0,
+						duration: 0.3,
+						delay: 0.3
+					})
+				})
+
+
+				// текст в секции почему нас выбирают
+				gsap.utils.toArray(".why-us__bottom-block-text").forEach((el) => {
+					gsap.set(el, { x: "-5.5rem" })
+				})
+
+
+				gsap.utils.toArray(".why-us__bottom-block-text").forEach((el) => {
+					let tl6 = gsap.timeline({
+						scrollTrigger: {
+							trigger: el,
+							start: "top bottom",
+							scrub: true,
+							scrub: 1,
+						}
+					});
+
+					tl6.to(el, {
+						x: 0,
+					})
+				})
+			},
+			"(max-width: 766px)": function () {
+
+			},
+			"all": function () {
+				let projectLayers = gsap.utils.toArray(".js-animated-layer");
+
+				function animFrom(elem, direction) {
+					direction = direction || -1;
+					var x = 0,
+						y = direction * 100;
+					gsap.fromTo(elem, {
+						y: 0,
+					}, {
+						delay: 0.6,
+						duration: 1.25,
+						y: `${y}%`,
+						ease: "expo",
+						overwrite: "auto"
+					});
+				}
+
+				projectLayers.forEach((el) => {
+
+					ScrollTrigger.create({
+						trigger: el,
+						onEnter: function () {
+							animFrom(el)
+						},
+						onEnterBack: function () {
+							animFrom(el, 1)
+						},
+						onLeave: function () {
+							gsap.set(el, { y: "100%" })
+						}
+					});
+				})
+			}
+
+		});
+	}
 
 }
