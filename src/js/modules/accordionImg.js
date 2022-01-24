92c2a1fd1bd282ead418742d37f5a94e3fdd68e7
Nail <nailgalimov8@gmail.com>
2022-01-24 15:54:49 +0300

    updts

--- src/js/modules/accordionImg.js
+++ src/js/modules/accordionImg.js
@@ -6,13 +6,24 @@ export default () => {
 		el.addEventListener("click", (element) => {
 			let attr = el.getAttribute('data-show');
 			attr = Number(attr);
+			let item = el.closest(".our-team-accordion__item");
+
 
 			if (el.className.includes('is-active')) {
 				container.querySelectorAll(".js-imgs").forEach((elmnt)=>{elmnt.classList.remove("mod-show");})
 				btn.forEach((btnel)=>{btnel.classList.remove("is-active");});
 			} else {
 				container.querySelectorAll(".js-imgs").forEach((elmnt)=>{elmnt.classList.remove("mod-show");})
-				container.querySelector(`.js-imgs[data-item='${attr}']`).classList.add("mod-show");
+
+				setTimeout(() => {
+					container.querySelector(`.js-imgs[data-item='${attr}']`).classList.add("mod-show");
+				}, 250);
+
+				if (window.matchMedia("(min-width: 1199px)").matches) {
+					setTimeout(() => {
+						container.querySelector(`.js-imgs[data-item='${attr}']`).style.top = item.offsetTop + "px";
+					}, 250);
+				}
 
 				btn.forEach((btnel)=>{btnel.classList.remove("is-active");});
 				el.classList.add("is-active");
